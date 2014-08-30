6f6a3aef2372a0bbcc29ec5b816b5f4a49bc4ca4
lexa <lexa@cfotr.com>
2014-08-31 00:09:15 +0400

    Removed usage of innerHTML in content script

--- data/panel-content.js
+++ data/panel-content.js
@@ -4,23 +4,23 @@ self.port.on("update_html", function (stats)
     document.getElementById("$2x").style.width = (stats.hp*100)/stats.maxHealth + "%";
     document.getElementById("$2z").style.width = (stats.exp*100)/stats.toNextLevel + "%";
 
-    document.getElementById("$2y").innerHTML = Math.round(stats.hp) + "/" + Math.round(stats.maxHealth);
-    document.getElementById("$2v").innerHTML = Math.round(stats.exp) + "/" + Math.round(stats.toNextLevel);
+    document.getElementById("$2y").textContent = Math.round(stats.hp) + "/" + Math.round(stats.maxHealth);
+    document.getElementById("$2v").textContent = Math.round(stats.exp) + "/" + Math.round(stats.toNextLevel);
 
-    document.getElementById("$au").innerHTML = Math.floor(stats.gp);
-    document.getElementById("$av").innerHTML = Math.round((stats.gp - Math.floor(stats.gp))*100);
+    document.getElementById("$au").textContent = Math.floor(stats.gp);
+    document.getElementById("$av").textContent = Math.round((stats.gp - Math.floor(stats.gp))*100);
 
-    document.getElementById("avatar-level").innerHTML = "Lvl " + stats.lvl;
+    document.getElementById("avatar-level").textContent = "Lvl " + stats.lvl;
     
     document.getElementById("splash_screen").style.display = 'none';
     document.getElementById("message").style.display = 'none';
-    document.getElementById("message").innerHTML = '';
+    document.getElementById("message").textContent = '';
 });
 
 self.port.on("show_error_message", function (message)
 {
     document.getElementById("message").style.display = 'block';
-    document.getElementById("message").innerHTML = message;
+    document.getElementById("message").textContent = String.quote(message);
     document.getElementById("splash_screen").style.display = 'none';
 });
 
